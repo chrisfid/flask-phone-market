8f672867ef8a101173464a59ad597e946d87d643
chrisfid <krzysztof.fidyka@gmail.com>
2021-05-08 19:52:20 +0200

    feat: authentication pt. 1

--- market/__init__.py
+++ market/__init__.py
@@ -1,9 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
 app.config['SECRET_KEY'] = 'c1b2ab50c74d1c749c1943f4'
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
 
 from market import routes
--- market/routes.py
+++ market/routes.py
@@ -1,7 +1,7 @@
 from market import app
 from flask import render_template, redirect, url_for, flash, get_flashed_messages
 from market.models import Item, User
-from market.forms import RegisterForm
+from market.forms import LoginForm, RegisterForm
 from market import db
 
 @app.route('/')
@@ -18,11 +18,19 @@ def market_page():
 def register_page():
     form = RegisterForm()
     if form.validate_on_submit():
-        user_to_create = User(username=form.username.data, email_address=form.email_address.data, password_hash=form.password1.data)
+        user_to_create = User(username=form.username.data,
+                              email_address=form.email_address.data, 
+                              password=form.password1.data)
         db.session.add(user_to_create)
         db.session.commit()
         return redirect(url_for('market_page'))
-    if form.errors != {}: # If there are no errors from validations
+    # If there are no errors from validations
+    if form.errors != {}:
         for err_msg in form.errors.values():
             flash(f'There was an error with creating a user: {err_msg}', category='danger')
     return render_template('register.html', form=form)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login_page():
+    form = LoginForm()
+    return render_template('login.html', form=form)
